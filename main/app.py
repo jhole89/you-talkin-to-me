d2b5056c3561a8b428be9f25405f4385e6b4e55b
Joel Lutman <joellutman@gmail.com>
2017-11-12 00:03:00 +0000

    load quote data a process into conversations

--- main/app.py
+++ main/app.py
@@ -1,4 +1,30 @@
 from chatterbot import ChatBot
+import gzip
+import os
+
+
+def _preprocess():
+    with gzip.open(
+            os.path.join('resources', 'moviequotes.scripts.txt.gz'), 'rt', encoding='latin-1') as gz_file:
+
+        all_conversations = []
+
+        for line in gz_file:
+            line_array = line.split('+++$+++')
+
+            line_dict = {
+                'line_id': line_array[0].strip(),
+                'reply_id': line_array[4].strip(),
+                'text': line_array[5].rstrip('\n').strip()
+            }
+
+            if line_dict['reply_id']:
+                if int(line_dict['reply_id']) - 1 == int(line_dict['line_id']):
+                    all_conversations[-1].append(line_dict['line_id'] + '---' + line_dict['text'])
+            else:
+                all_conversations.append([line_dict['text']])
+
+        return all_conversations
 
 
 def run():
@@ -6,6 +32,10 @@ def run():
     chatbot = ChatBot(
         name='Swan Ronson',
         trainer='chatterbot.trainers.ListTrainer')
+
+    training_dialog = _preprocess()
+
+    print(training_dialog)
 
     chatbot.train([
         "Hi",
@@ -15,7 +45,7 @@ def run():
     ])
 
     # Get a response to the input text 'How are you?'
-    response = chatbot.get_response("Hi")
+    response = chatbot.get_response("I'm fine")
 
     print(response)
 
